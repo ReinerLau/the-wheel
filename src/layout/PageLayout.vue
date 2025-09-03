2f49cae10e574f38f21b13fe77fe5dc66859ae6c
ReinerLau <lk850593913@gmail.com>
2025-09-03 09:01:08 +0800

    Remove unused LayoutSidebar component from PageLayout.vue

--- src/layout/PageLayout.vue
+++ src/layout/PageLayout.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="h-full flex">
-    <LayoutSidebar />
     <div class="flex-1 flex flex-col min-h-0">
       <LayoutNavbar />
       <AppMain class="flex-1 bg-gray-100 p-10 min-h-0" />
@@ -11,5 +10,4 @@
 <script setup lang="ts">
 import AppMain from './AppMain.vue'
 import LayoutNavbar from './LayoutNavbar.vue'
-import LayoutSidebar from './LayoutSidebar.vue'
 </script>
