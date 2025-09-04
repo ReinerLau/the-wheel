2b20218e71573cdeae80723ffaa7a043eba0d91d
ReinerLau <lk850593913@gmail.com>
2025-09-04 15:51:11 +0800

    Refactor router configuration by consolidating constant routes; remove unused routes and streamline the routing setup for improved maintainability and clarity.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,64 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import type { RouteRecordRaw } from 'vue-router'
-import PageLayout from '../layout/PageLayout.vue'
 import physiological from './modules/physiological'
 import settings from './modules/settings'
 
-export const constRoutes: RouteRecordRaw[] = [
-  {
-    path: '/',
-    name: 'home',
-    component: PageLayout,
-    redirect: { name: 'monitor-index' },
-    meta: {
-      title: '首页',
-      root: true
-    },
-    children: [
-      {
-        path: 'monitor',
-        name: 'monitor-index',
-        component: () => import('@/views/monitor/MonitorIndex.vue'),
-        meta: {
-          title: '首页'
-        }
-      }
-    ]
-  },
-  {
-    path: '/mobile',
-    name: 'mobile-monitor',
-    component: () => import('@/views/monitor/MobileMonitorIndex.vue'),
-    meta: {
-      title: '首页',
-      hidden: true
-    }
-  },
-  {
-    path: '/login',
-    name: 'login',
-    component: () => import('../views/LoginPage.vue'),
-    meta: {
-      title: '登录',
-      hidden: true
-    }
-  }
-]
-
-/**
- * 动态路由数组
- */
-export const asyncRoutes = [physiological, settings]
+export const constRoutes: RouteRecordRaw[] = [physiological, settings]
 
 const router = createRouter({
   history: createWebHistory(),
-  routes: [...constRoutes, ...asyncRoutes]
+  routes: constRoutes
 })
-
-export function resetRouter() {
-  asyncRoutes.forEach((route) => {
-    router.removeRoute(route.name)
-  })
-}
 
 export default router
